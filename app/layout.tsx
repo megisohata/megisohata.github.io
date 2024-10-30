17b1f3e885d845d56e9b63d5389c5423817753d0
Meg <msi32@cornell.edu>
2024-10-29 23:16:43 -0400

    Finish experience section of homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,13 +15,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-cream">
-        <div className="px-16 py-8 w-screen h-screen">
+        <div className="px-16 py-8 w-screen">
           <main className="flex flex-col gap-8 w-full h-full">
             <div className="bg-forest text-cream px-4 flex items-center justify-between w-full h-12 rounded-lg shrink-0">
               <a href="/" className="text-2xl"><span className="spin">🌼</span><span className="spin">🌼</span><span className="spin">🌼</span></a>
               <a href="/about" className="spinContainer"><span className="spinElement">❀</span> About <span className="spinElement">❀</span></a>
-              <a href="/experience" className="spinContainer"><span className="spinElement">✿</span> Experience <span className="spinElement">✿</span></a>
-              <a href="/portfolio" className="spinContainer"><span className="spinElement">✧</span> Portfolio <span className="spinElement">✧</span></a>
+              <a href="/experience" className="spinContainer"><span className="spinElement">✧</span> Experience <span className="spinElement">✧</span></a>
+              <a href="/portfolio" className="spinContainer"><span className="spinElement">✿</span> Portfolio <span className="spinElement">✿</span></a>
               <div className="flex gap-1">
                 <a href="https://github.com/megisohata" target="_blank"><img src="github.svg" className="h-8 w-8" /></a>
                 <a href="https://www.linkedin.com/in/meg-isohata/" target="_blank"><img src="linkedin.svg" className="h-8 w-8" /></a>
--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,58 @@ export default function Home() {
       <div className="flex-1 flex flex-col gap-4">
         <div className="flex-1 flex flex-col bg-pistachio rounded-lg p-8 justify-center items-center">
           <h1 className="text-2xl">Experience</h1>
-          <a href="/experience" className="py-2 px-4 my-2 border-2 border-forest rounded-sm spinContainer"><span className="spinElement">✿</span> Full Experience <span className="spinElement">✿</span></a>
+          <div className="flex flex-col my-4 w-full">
+            <div className="flex flex-col">
+              <div className="flex justify-between items-center">
+                <h1 className="text-base">Autopilot Software Engineer</h1>
+                <p className="text-base">OCT 2023 - Present</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <a href="https://cuair.org/" target="_blank" className="bg-cream w-fit py-0.5 px-1 rounded-md text-sm italic">🔗 Cornell University Unmanned Air Systems</a>
+                <p className="text-sm">Ithaca, NY</p>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="flex flex-col my-4 w-full">
+            <div className="flex flex-col">
+              <div className="flex justify-between items-center">
+                <h1 className="text-base">Python Course Consultant</h1>
+                <p className="text-base">JUL 2024 - Present</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <a className="text-sm italic">Bowers College of Computing and Information Science</a>
+                <p className="text-sm">Ithaca, NY</p>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="flex flex-col my-4 w-full">
+            <div className="flex flex-col">
+              <div className="flex justify-between items-center">
+                <h1 className="text-base">Corporate Relations Director</h1>
+                <p className="text-base">JAN 2024 - Present</p>
+              </div>
+              <div className="flex justify-between items-center">
+              <a href="https://wicc.cornell.edu/" target="_blank" className="bg-cream w-fit py-0.5 px-1 rounded-md text-sm italic">🔗 Women in Computing at Cornell</a>
+                <p className="text-sm">Ithaca, NY</p>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="flex flex-col my-4 w-full">
+            <div className="flex flex-col">
+              <div className="flex justify-between items-center">
+                <h1 className="text-base">Laboratory Technician</h1>
+                <p className="text-base">OCT 2023 - Present</p>
+              </div>
+              <div className="flex justify-between items-center">
+              <a href="https://rpl.engineering.cornell.edu/" target="_blank" className="bg-cream w-fit py-0.5 px-1 rounded-md text-sm italic">🔗 Cornell Rapid Prototyping Lab</a>
+                <p className="text-sm">Ithaca, NY</p>
+              </div>
+            </div>
+          </div>
+          <a href="/experience" className="py-2 px-4 my-2 border-2 border-forest rounded-sm spinContainer"><span className="spinElement">✧</span> View Full Experience <span className="spinElement">✧</span></a>
         </div>
         <div className="h-40 bg-pistachio border-2 border-forest rounded-lg"></div>
       </div>
@@ -24,7 +75,7 @@ export default function Home() {
         <div className="h-40 bg-pistachio border-2 border-forest rounded-lg"></div>
         <div className="flex-1 flex flex-col bg-pistachio rounded-lg p-8 justify-center items-center">
           <h1 className="text-2xl">Portfolio</h1>
-          <a href="/portfolio" className="py-2 px-4 my-2 border-2 border-forest rounded-sm spinContainer"><span className="spinElement">✧</span> All Projects <span className="spinElement">✧</span></a>
+          <a href="/portfolio" className="py-2 px-4 my-2 border-2 border-forest rounded-sm spinContainer"><span className="spinElement">✿</span> See All Projects <span className="spinElement">✿</span></a>
         </div>
       </div>
     </div>
