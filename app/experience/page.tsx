19ddc74fb43d924054bc6ab4076926b96b111fd0
Meg <msi32@cornell.edu>
2024-11-11 15:18:17 -0500

    Revert to working state

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,6 +1,4 @@
 import ExperienceItem from "../experience-item";
-import SlideIn from "../slide-in";
-import FadeIn from "../fade-in";
 
 interface ExperienceEntry {
   title: string;
@@ -129,107 +127,84 @@ const experiences: ExperienceEntry[] = [
 const Experience: React.FC = () => {
   return (
     <div className="flex flex-col gap-8 w-2/3 mx-auto">
-      <SlideIn>
-        <div className="bg-pistachio p-8 w-full rounded-lg">
-          <h1 className="text-2xl">Education</h1>
-          <FadeIn>
-            <div className="flex flex-col my-2 w-full">
-              <div className="flex justify-between items-center">
-                <h1 className="text-base">
-                  Cornell University | College of Engineering
-                </h1>
-                <p className="text-base">May 2027</p>
-              </div>
-              <div className="flex justify-between items-center">
-                <p className="text-base">
-                  Bachelor of Science, Computer Science | Minor, Artificial
-                  Intelligence
-                </p>
-                <p className="text-base italic">Ithaca, NY</p>
-              </div>
-              <div className="pl-4">
-                <p className="text-base">
-                  <span className="italic">Relevant Coursework: </span>Data
-                  Structures and Functional Programming, Object-Oriented
-                  Programming and Data Structures, Mathematical Foundations of
-                  Computing, Engineering Probability and Statistics, Probability
-                  Models and Inference, Engineering Applications of Operations
-                  Research, Linear Algebra, Multivariable Calculus
-                </p>
-              </div>
-            </div>
-          </FadeIn>
-          <FadeIn>
-            <div className="flex flex-col my-2 w-full">
-              <div className="flex justify-between items-center">
-                <h1 className="text-base">
-                  Yonsei University | International Summer School
-                </h1>
-                <p className="text-base">JUN 2024 - AUG 2024</p>
-              </div>
-              <div className="flex justify-between items-center">
-                <p className="text-base">Study Abroad</p>
-                <p className="text-base italic">Seoul, KR</p>
-              </div>
-              <div className="pl-4">
-                <p className="text-base">
-                  Summer 2024 semester abroad, studying Korean language,
-                  culture, and history and engaging in immersive cultural
-                  experiences to enhance language skills and deepen cultural
-                  understanding.
-                </p>
-              </div>
-            </div>
-          </FadeIn>
+      <div className="bg-pistachio p-8 w-full rounded-lg">
+        <h1 className="text-2xl">Education</h1>
+        <div className="flex flex-col my-2 w-full">
+          <div className="flex justify-between items-center">
+            <h1 className="text-base">
+              Cornell University | College of Engineering
+            </h1>
+            <p className="text-base">May 2027</p>
+          </div>
+          <div className="flex justify-between items-center">
+            <p className="text-base">
+              Bachelor of Science, Computer Science | Minor, Artificial
+              Intelligence
+            </p>
+            <p className="text-base italic">Ithaca, NY</p>
+          </div>
+          <div className="pl-4">
+            <p className="text-base">
+              <span className="italic">Relevant Coursework: </span>Data
+              Structures and Functional Programming, Object-Oriented Programming
+              and Data Structures, Mathematical Foundations of Computing,
+              Engineering Probability and Statistics, Probability Models and
+              Inference, Engineering Applications of Operations Research, Linear
+              Algebra, Multivariable Calculus
+            </p>
+          </div>
         </div>
-      </SlideIn>
-      <SlideIn>
-        <div className="bg-pistachio p-8 w-full rounded-lg">
-          <h1 className="text-2xl">Technical Skills</h1>
-          <FadeIn>
-            <div className="flex gap-2">
-              <h1 className="italic">Programming Languages: </h1>
-              <p>Java, Python, JavaScript, OCaml</p>
-            </div>
-          </FadeIn>
-          <FadeIn>
-            <div className="flex gap-2">
-              <h1 className="italic">Technologies & Frameworks: </h1>
-              <p>HTML, CSS, Svelte, React, Next, Tailwind CSS, Docker</p>
-            </div>
-          </FadeIn>
-          <FadeIn>
-            <div className="flex gap-2">
-              <h1 className="italic">Tools: </h1>{" "}
-              <p>Git, GitHub, Visual Studio Code</p>
-            </div>
-          </FadeIn>
-          <FadeIn>
-            <div className="flex gap-2">
-              <h1 className="italic">Other: </h1>
-              <p>SolidWorks, Onshape</p>
-            </div>
-          </FadeIn>
+        <div className="flex flex-col my-2 w-full">
+          <div className="flex justify-between items-center">
+            <h1 className="text-base">
+              Yonsei University | International Summer School
+            </h1>
+            <p className="text-base">JUN 2024 - AUG 2024</p>
+          </div>
+          <div className="flex justify-between items-center">
+            <p className="text-base">Study Abroad</p>
+            <p className="text-base italic">Seoul, KR</p>
+          </div>
+          <div className="pl-4">
+            <p className="text-base">
+              Summer 2024 semester abroad, studying Korean language, culture,
+              and history and engaging in immersive cultural experiences to
+              enhance language skills and deepen cultural understanding.
+            </p>
+          </div>
         </div>
-      </SlideIn>
-      <SlideIn>
-        <div className="bg-pistachio p-8 w-full rounded-lg">
-          <h1 className="text-2xl">Experience</h1>
-          {experiences.map((exp, index) => (
-            <FadeIn key={index}>
-              <div>
-                <ExperienceItem {...exp} />
-                {index < experiences.length - 1 && <hr />}
-              </div>
-            </FadeIn>
-          ))}
+      </div>
+      <div className="bg-pistachio p-8 w-full rounded-lg">
+        <h1 className="text-2xl">Technical Skills</h1>
+        <div className="flex gap-2">
+          <h1 className="italic">Programming Languages: </h1>
+          <p>Java, Python, JavaScript, OCaml</p>
         </div>
-      </SlideIn>
-      <SlideIn>
-        <div className="bg-pistachio p-8 w-full rounded-lg">
-          <h1 className="text-2xl">Awards</h1>
+        <div className="flex gap-2">
+          <h1 className="italic">Technologies & Frameworks: </h1>
+          <p>HTML, CSS, Svelte, React, Next, Tailwind CSS, Docker</p>
         </div>
-      </SlideIn>
+        <div className="flex gap-2">
+          <h1 className="italic">Tools: </h1>{" "}
+          <p>Git, GitHub, Visual Studio Code</p>
+        </div>
+        <div className="flex gap-2">
+          <h1 className="italic">Other: </h1>
+          <p>SolidWorks, Onshape</p>
+        </div>
+      </div>
+      <div className="bg-pistachio p-8 w-full rounded-lg">
+        <h1 className="text-2xl">Experience</h1>
+        {experiences.map((exp, index) => (
+          <div key={index}>
+            <ExperienceItem {...exp} />
+            {index < experiences.length - 1 && <hr />}
+          </div>
+        ))}
+      </div>
+      <div className="bg-pistachio p-8 w-full rounded-lg">
+        <h1 className="text-2xl">Awards</h1>
+      </div>
     </div>
   );
 };
--- app/portfolio/portfolio-item.tsx
+++ app/portfolio/portfolio-item.tsx
@@ -1,6 +1,3 @@
-import FadeIn from "../fade-in";
-import SlideIn from "../slide-in";
-
 interface PortfolioItemProps {
   title: string;
   date: string;
@@ -19,47 +16,43 @@ const PortfolioItem: React.FC<PortfolioItemProps> = ({
   githubLink,
 }) => {
   return (
-    <SlideIn>
-      <div className="bg-pistachio p-8 rounded-md h-fit">
-        <FadeIn>
-          <img
-            src={imgSrc}
-            alt={title}
-            className="w-full h-auto object-cover rounded-md"
-          />
+    <div className="bg-pistachio p-8 rounded-md h-fit">
+      <img
+        src={imgSrc}
+        alt={title}
+        className="w-full h-auto object-cover rounded-md"
+      />
 
-          <div className="flex justify-between items-center my-4">
-            <h1 className="text-2xl">{title}</h1>
-            <p className="text-base">{date}</p>
-          </div>
-
-          <div className="flex gap-2 items-center my-2">
-            {liveDemoLink && (
-              <a
-                href={liveDemoLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-cream w-fit py-0.5 px-1 rounded-md text-base italic"
-              >
-                🔗 Live Demo
-              </a>
-            )}
-            {githubLink && (
-              <a
-                href={githubLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex justify-center items-center w-6 h-6 rounded-full bg-forest"
-              >
-                <img src="github.svg" alt="GitHub" />
-              </a>
-            )}
-          </div>
-
-          <p className="text-base">{description}</p>
-        </FadeIn>
+      <div className="flex justify-between items-center my-4">
+        <h1 className="text-2xl">{title}</h1>
+        <p className="text-base">{date}</p>
       </div>
-    </SlideIn>
+
+      <div className="flex gap-2 items-center my-2">
+        {liveDemoLink && (
+          <a
+            href={liveDemoLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-cream w-fit py-0.5 px-1 rounded-md text-base italic"
+          >
+            🔗 Live Demo
+          </a>
+        )}
+        {githubLink && (
+          <a
+            href={githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex justify-center items-center w-6 h-6 rounded-full bg-forest"
+          >
+            <img src="github.svg" alt="GitHub" />
+          </a>
+        )}
+      </div>
+
+      <p className="text-base">{description}</p>
+    </div>
   );
 };
 
