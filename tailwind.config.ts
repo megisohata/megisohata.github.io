5b445a68e02ed7ccd94bc6c3198f1c38a1ae55e0
Meg <msi32@cornell.edu>
2024-12-25 21:10:17 -0800

    finish layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,24 +2,24 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        "cream": "#f4f1e5",
-        "pistachio": "#dce2cf",
-        "forest": "#012627"
+        cream: "#f4f1e5",
+        pistachio: "#dce2cf",
+        forest: "#012627",
       },
-      fontFamily:{
+      fontFamily: {
         dosis: ["Dosis", "Verdana", "sans-serif"],
-        jua: ["Jua", "Times", "serif"]
+        jua: ["Jua", "Times", "serif"],
       },
       animation: {
-        'spin-slow': 'spin 3s linear infinite',
-      }
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
   },
   plugins: [],
