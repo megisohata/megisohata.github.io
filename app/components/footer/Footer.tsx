c5c2b22073a48acc119ada01e8b3f076b42b43e8
Meg <msi32@cornell.edu>
2026-04-05 14:50:47 -0700

    Fresh start

--- app/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-brown text-cream w-full px-4 py-4 text-center">
-      <p>Made with care, curiosity, and caffeine. ☕️</p>
-      <p>✰ ✰ ✰ ✰</p>
-      <p>© 2025 Meg Isohata 🌱✨</p>
-    </footer>
-  );
-}
